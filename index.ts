62641f5a0fd9bf3d5ad4696be18b59c70cf8bb29
avinash <avinashsharma17012002@gmail.com>
2024-12-19 11:15:11 +0530

    initial set up

---
+++ index.ts
@@ -0,0 +1,27 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
+
+const server = http.createServer(function(request: any, response: any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.end("hi there");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  ws.send('Hello! Message From Server!!');
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
